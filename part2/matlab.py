b51dfa1da872b976cd0fb66be8d21eb27ead4e17
1open1 <random10000006@gmail.com>
2025-05-21 14:19:41 +0530

    .

--- part2/matlab.py
+++ part2/matlab.py
@@ -1,29 +1,18 @@
-% Channel details
-channelID = 2934028;
+channelID = 2934028; 
 readAPIKey = '57HB9YD28FI7C4VF';
-fieldNumber = 1; % Use 2 for Data2 if needed
+writeAPIKey = 'YOUR_WRITE_API_KEY'; data = thingSpeakRead(channelID, "NumPoints", 100, "ReadKey", readAPIKey); 
+thingSpeakWrite(channelID, "Fields", [1, 2, 3, 4], "Values", [max(data), min(data), mean(data), std(data)], "WriteKey", writeAPIKey);
 
-% Read the last 100 points from the specified field
-data = thingSpeakRead(channelID, ...
-    "Fields", fieldNumber, ...
-    "NumPoints", 100, ...
-    "ReadKey", readAPIKey);
-
-% Calculate statistics
 maxval = max(data);
 minval = min(data);
 meanval = mean(data);
 stdval = std(data);
 
-% Display the results
 disp("Maximum Value:");
 disp(maxval);
-
 disp("Minimum Value:");
 disp(minval);
-
 disp("Mean Value:");
 disp(meanval);
-
 disp("Standard Deviation:");
 disp(stdval);
