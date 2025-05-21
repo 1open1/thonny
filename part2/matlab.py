207e627eab421d9f06d2be2be5738af051ce12d6
1open1 <random10000006@gmail.com>
2025-05-21 14:20:01 +0530

    .

--- part2/matlab.py
+++ part2/matlab.py
@@ -1,6 +1,7 @@
 channelID = 2934028; 
 readAPIKey = '57HB9YD28FI7C4VF';
-writeAPIKey = 'YOUR_WRITE_API_KEY'; data = thingSpeakRead(channelID, "NumPoints", 100, "ReadKey", readAPIKey); 
+writeAPIKey = 'YOUR_WRITE_API_KEY'; 
+data = thingSpeakRead(channelID, "NumPoints", 100, "ReadKey", readAPIKey); 
 thingSpeakWrite(channelID, "Fields", [1, 2, 3, 4], "Values", [max(data), min(data), mean(data), std(data)], "WriteKey", writeAPIKey);
 
 maxval = max(data);
