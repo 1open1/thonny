5a2e94176c7746e82cbdaa6099bd8ccba479f4ed
1open1 <random10000006@gmail.com>
2025-05-22 10:27:29 +0530

    .

--- part2/master.py
+++ part2/master.py
@@ -192,26 +192,29 @@ disp(stdval);
 
 # custom input
 import RPi.GPIO as GPIO
-import time
+from time import sleep
+
 GPIO.setmode(GPIO.BOARD)
 GPIO.setwarnings(False)
-pin = 11
-GPIO.setup(pin, GPIO.OUT)
-pwm = GPIO.PWM(pin, 50)
-pwm.start(5)
-print("Waiting for 1 second...")
 
-time.sleep(1)
-duty = 2
-while duty <= 17:
-    pwm.ChangeDutyCycle(duty)
-    time.sleep(5)
-    duty = duty + 2
+control_pin = 10
+GPIO.setup(control_pin, GPIO.OUT)  # connect to enable pin-1 of Driver IC
 
-print("Turning back to 0 degrees")
-pwm.ChangeDutyCycle(2)
-time.sleep(5)
+pwm = GPIO.PWM(control_pin, 50)  # to setup the pwm commands type 50Hz frequency
+
+set_angle = int(input("Enter Servo Motor Rotation Angle: "))
+
+pwm.start(0)
+delay = 1
+duty_cycle = (set_angle / 18) + 2.5
+
+GPIO.output(control_pin, True)
+pwm.ChangeDutyCycle(duty_cycle)
+# GPIO.output(control_pin, True)
+sleep(delay)
+
+GPIO.output(control_pin, False)
 pwm.ChangeDutyCycle(0)
+
 pwm.stop()
 GPIO.cleanup()
-print("Everything is cleaned")
